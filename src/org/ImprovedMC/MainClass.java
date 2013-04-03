f7998070cf15a76845de7fad33fdc1d71a9f1df1
ImprovedMC Developer Network <info@improvedmc.org>
2013-04-03 21:38:40 +0300

    Update MainClass.java

--- src/org/ImprovedMC/MainClass.java
+++ src/org/ImprovedMC/MainClass.java
@@ -1,88 +1,3 @@
-<<<<<<< HEAD
-package org.ImprovedMC;
-
-import java.util.logging.Logger;
-
-import org.ImprovedMC.Chat.CommandEvent;
-import org.ImprovedMC.Chat.Command_Clear;
-import org.ImprovedMC.Until.CgM;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class MainClass extends JavaPlugin{
-
-	Logger log = Logger.getLogger("Minecraft");
-	
-	private String pluginPrefix = "�e[ChatProvider] �a";
-	
-	private String msgC = "messages.yml";
-	private String gloC = "config.yml";
-	
-	@Override
-	public void onDisable() {
-		
-	}
-	
-	@Override
-	public void onEnable() {
-		/**
-		 * Create Default Configs
-		 */
-		CgM.addConfig(this, msgC);
-		CgM.addConfig(this, gloC);
-		
-		
-		try {
-			CgM.getConfig(msgC).options().copyDefaults(true);
-			CgM.getConfig(gloC).options().copyDefaults(true);
-		} catch(NullPointerException e){
-			sendConsole("Configs are Ready!");
-		}
-		CgM.saveConfigs(this);
-		
-		getCommand("clear").setExecutor(new Command_Clear(this));
-		getServer().getPluginManager().registerEvents(new CommandEvent(this), this);
-		
-		sendConsole("Plugin is Ready!");
-		
-	}
-	
-	
-	
-	public void sendConsole(String msg){
-		log.info("�e[ChatProvider] �a" + msg);
-	}
-	
-	public String replaceColor(String msg){
-		return ChatColor.translateAlternateColorCodes('&', msg);
-	}
-	
-	public String getPrefix(){
-		return pluginPrefix;
-	}
-	
-	@Deprecated
-	public boolean hasPermissions(MainClass mClass, Player p, String permission){
-		if(p.hasPermission(permission)){
-			mClass.sendConsole(mClass.getPrefix() + "Player " + p.getName() + " have Permission: " + permission);
-			return true;
-		} else {
-			mClass.sendConsole(mClass.getPrefix() + "Player " + p.getName() + " dont have Permission: " + permission);
-			return false;
-		}
-	}
-	
-	public boolean hasPerm(Player p, String permission){
-		if(p.hasPermission(permission)){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-}
-=======
 package org.ImprovedMC;
 
 import java.io.FileNotFoundException;
@@ -173,7 +88,7 @@ public class MainClass extends JavaPlugin implements Listener{
 		Player p = e.getPlayer();
 		ArrayList<String> messages = new ArrayList<String>();
 		messages.add("§6▃▄▅▆▇█");
-		messages.add("§6DiaPVP.me");
+		messages.add("§6SERVERNAME");
 		messages.add("§6█▇▆▅▄▃▂▁");
 		messages.add("§eWilkommen");
 		messages.add("§a" + p.getName());
@@ -202,4 +117,3 @@ public class MainClass extends JavaPlugin implements Listener{
 	}
 	
 }
->>>>>>> UPDATE + ADD LANG & TAB API
