8edf6badf26e663d44d2f642b656ddc61adec088
ImprovedMC Developer Network <info@improvedmc.org>
2013-04-03 21:39:35 +0300

    Update pluginException.java

--- src/org/ImprovedMC/Until/pluginException.java
+++ src/org/ImprovedMC/Until/pluginException.java
@@ -1,12 +1,9 @@
 package org.ImprovedMC.Until;
 
-<<<<<<< HEAD
-=======
 @Deprecated
->>>>>>> UPDATE + ADD LANG & TAB API
 public enum pluginException {
 
-	NO_PERMISSIONS_EXCEPTION("§cYou dont have Permission to Acces This Command"),
+	NO_PERMISSIONS_EXCEPTION("ï¿½cYou dont have Permission to Acces This Command"),
 	GLOBAL_CHAT_CLEARED("Global chat cleared by "),
 	LOCAL_CHAT_CLEARED("You have been cleared your local chat.");
 	public String msg;
