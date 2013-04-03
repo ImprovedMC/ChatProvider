d56c195d3b80acd2cb1db0ff0bf8e4c8dbe4dfea
ImprovedMC Developer Network <info@improvedmc.org>
2013-04-03 23:16:10 +0300

    Update Optimize.java

--- src/org/ImprovedMC/Until2/Optimize.java
+++ src/org/ImprovedMC/Until2/Optimize.java
@@ -3,8 +3,8 @@ package org.ImprovedMC.Until2;
 public @interface Optimize {
 
 	enum Priority { LOW, NORMAL, HIGH }
-	String   value() default "Command";
-    String[] assignedTo() default "/info";
-    Priority   priority();
+	String value() default "Command";
+	String[] assignedTo() default "/info";
+	Priority priority();
 	
 }
